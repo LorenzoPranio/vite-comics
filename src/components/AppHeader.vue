76a99c20ee390b6388653b541606ada4f258a542
LorenzoPranio <lorenzopranio02@gmail.com>
2024-07-29 16:48:08 +0200

    [UPD]Added elements in AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -6,7 +6,27 @@ export default {
 
 <template>
     <div>
-        
+        <div class="container">
+            <div class="row">
+                <div class="col-4 justify-content-start align-self-center p-4">
+                    <img src="../assets/dc-logo.png" alt="">
+                </div>
+                <div class="col-8 justify-content-end align-self-center p-4">
+                    <ul class="list-unstyled d-flex">
+                        <li class="mx-3"><a href="#">CHARACTERS</a></li>
+                        <li class="mx-3"><a href="#">COMICS</a></li>
+                        <li class="mx-3"><a href="#">MOVIE</a></li>
+                        <li class="mx-3"><a href="#">TV</a></li>
+                        <li class="mx-3"><a href="#">GAMES</a></li>
+                        <li class="mx-3"><a href="#">COLLECTIBLES</a></li>
+                        <li class="mx-3"><a href="#">VIDEOS</a></li>
+                        <li class="mx-3"><a href="#">FANS</a></li>
+                        <li class="mx-3"><a href="#">NEWS</a></li>
+                        <li class="mx-3"><a href="#">SHOP</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
